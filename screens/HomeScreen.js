93ec5caec9c1470ad9b40fcb77359004794fc8a6
Dan Bellinski <bellindj@gmail.com>
2018-12-01 10:28:11 -0500

    initial alert for reminders

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,29 +1,30 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import SplashScreen from "react-native-splash-screen";
-import { connect } from "react-redux";
-import AnytimeTasksTile from "../components/AnytimeTasksTile";
-import { EverydayTasksTile } from "../components/EverydayTasksTile";
-import LearnHowTile from "../components/LearnHowTile";
-import MindfulQuoteTile from "../components/MindfulQuoteTile";
-import { ScrollingPageContainer } from "../components/ScrollingPageContainer";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+import { connect } from 'react-redux';
+import AnytimeTasksTile from '../components/AnytimeTasksTile';
+import { EverydayTasksTile } from '../components/EverydayTasksTile';
+import LearnHowTile from '../components/LearnHowTile';
+import MindfulQuoteTile from '../components/MindfulQuoteTile';
+import { ScrollingPageContainer } from '../components/ScrollingPageContainer';
+import TutorialView from '../components/TutorialView';
 import {
   updateIAPs,
   updateUserSubscriptions
-} from "../redux/reducers/subscription";
-import { updateTasks } from "../redux/reducers/tasks";
+} from '../redux/reducers/subscription';
+import { updateTasks } from '../redux/reducers/tasks';
 // TODO add stars to tile if have done one of the tasks today. Make BG lighter?
 
 class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: "Home"
+    title: 'Home'
   };
 
   componentDidMount() {
     this.props.updateTasks();
     this.props.updateIAPs();
-    if (!this.props.tutorial["appIntro"]) {
-      this.props.navigation.navigate("Intro");
+    if (!this.props.tutorial['appIntro']) {
+      this.props.navigation.navigate('Intro');
     }
     SplashScreen.hide();
   }
@@ -39,15 +40,20 @@ class HomeScreen extends React.Component {
 
     return (
       <ScrollingPageContainer>
-        {/* <TutorialView
-          {...this.props}
-          tutorialNavigation={"FAQ"}
-          tutorialType="homeIntro"
-          tutorialTitle="Welcome to Present Sense!"
-          tutorialDescription={
-            "Start with the tutorials in the Learn How section to continue your learning journey. If you want to learn the 'why' behind mindfulness, see the FAQ section in Settings."
-          }
-        /> */}
+        {!this.props.remindersEnabled &&
+        this.props.completedTasks.length > 0 ? (
+          <TutorialView
+            {...this.props}
+            tutorialNavigation={'Settings'}
+            tutorialType="reminders"
+            tutorialTitle="Setup a Daily Reminder!"
+            tutorialDescription={
+              'Let us help you remember to find your mindful moments. Visit Settings to enable these.'
+            }
+          />
+        ) : (
+          <View />
+        )}
         <MindfulQuoteTile />
         <LearnHowTile {...this.props} />
         <EverydayTasksTile {...this.props} />
@@ -62,7 +68,9 @@ const styles = StyleSheet.create({});
 const mapStateToProps = state => {
   return {
     tutorial: state.tutorial.tutorial,
-    activeTask: state.tasks.activeTask
+    activeTask: state.tasks.activeTask,
+    remindersEnabled: state.notification.remindersEnabled,
+    completedTasks: state.tasks.completedTasks
   };
 };
 
