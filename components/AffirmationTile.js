2e7bc9348f09e3e7ef4a40bafcce07b90d5c5881
Dan Bellinski <bellindj@gmail.com>
2018-12-07 08:17:44 -0500

    Affirmations working using the quote data

--- components/AffirmationTile.js
+++ components/AffirmationTile.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { quoteData } from '../redux/reducers/quote-data';
 import {
@@ -13,74 +14,100 @@ import {
 import { MyText } from './MyText';
 
 class AffirmationTile extends React.Component {
-  quoteNumber = Math.floor(Math.random() * quoteData.length);
+  quoteNumber = this.getRandomQuoteNumber();
+
+  getNewAffirmation() {
+    this.quoteNumber = this.getRandomQuoteNumber();
+    this.forceUpdate();
+  }
+
+  getRandomQuoteNumber() {
+    return Math.floor(Math.random() * quoteData.length);
+  }
 
   render() {
     return (
       <View
         style={[
           styles.header,
-          { backgroundColor: getBackgroundColor(this.quoteNumber) }
+          { backgroundColor: this.getBackgroundColor(this.quoteNumber) }
         ]}
       >
-        <MyText
-          style={[
-            styles.quoteText,
-            !!quoteData[this.quoteNumber].author
-              ? { paddingBottom: 20 }
-              : { paddingBottom: 0 }
-          ]}
-        >
-          {quoteData[this.quoteNumber].quote}
-        </MyText>
-        {!!quoteData[this.quoteNumber].author ? (
-          <MyText style={styles.quoteAuthor}>
-            - {quoteData[this.quoteNumber].author} -
+        <View>
+          <MyText
+            style={[
+              styles.quoteText,
+              !!quoteData[this.quoteNumber].author
+                ? { paddingBottom: 20 }
+                : { paddingBottom: 0 }
+            ]}
+          >
+            {quoteData[this.quoteNumber].quote}
           </MyText>
-        ) : (
-          <View />
-        )}
+          {!!quoteData[this.quoteNumber].author ? (
+            <MyText style={styles.quoteAuthor}>
+              - {quoteData[this.quoteNumber].author} -
+            </MyText>
+          ) : (
+            <View />
+          )}
+          <View style={styles.refresh}>
+            <TouchableOpacity onPress={() => this.getNewAffirmation()}>
+              <Icon
+                type="material"
+                color={COLOR_WHITE}
+                name="refresh"
+                size={30}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
-}
 
-getBackgroundColor = function(quoteNumber) {
-  switch (quoteNumber % 5) {
-    case 0:
-      return COLOR_ALERT;
-    case 1:
-      return COLOR_SECONDARY;
-    case 2:
-      return COLOR_TERTIARY;
-    case 3:
-      return COLOR_HIGHLIGHT;
-    default:
-      return COLOR_PRIMARY;
-  }
-};
+  getBackgroundColor = function(quoteNumber) {
+    switch (quoteNumber % 5) {
+      case 0:
+        return COLOR_ALERT;
+      case 1:
+        return COLOR_SECONDARY;
+      case 2:
+        return COLOR_TERTIARY;
+      case 3:
+        return COLOR_HIGHLIGHT;
+      default:
+        return COLOR_PRIMARY;
+    }
+  };
+}
 
 const styles = StyleSheet.create({
   header: {
     backgroundColor: COLOR_WHITE,
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     flexDirection: 'column',
     alignItems: 'center',
     padding: 30
   },
   quoteText: {
-    flex: 1,
+    // flex: 1,
     textAlign: 'center',
-
     color: COLOR_WHITE,
     fontSize: 18
   },
   quoteAuthor: {
-    flex: 1,
+    // flex: 1,
     textAlign: 'center',
     color: COLOR_WHITE,
     fontSize: 16
+  },
+  refresh: {
+    alignItems: 'flex-end',
+    marginTop: 40,
+    paddingLeft: 20,
+    paddingRight: 20
   }
 });
 
--- components/MindfulQuoteTile.js
+++ components/MindfulQuoteTile.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { connect } from "react-redux";
-import { quoteData } from "../redux/reducers/quote-data";
-import { COLOR_PRIMARY, COLOR_WHITE } from "../styles/common";
-import { MyText } from "./MyText";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { connect } from 'react-redux';
+import { quoteData } from '../redux/reducers/quote-data';
+import { COLOR_PRIMARY, COLOR_WHITE } from '../styles/common';
+import { MyText } from './MyText';
 
 class MindfulQuoteTile extends React.Component {
   quoteNumber = Math.floor(Math.random() * quoteData.length);
@@ -13,7 +13,7 @@ class MindfulQuoteTile extends React.Component {
       <View
         style={[
           styles.header,
-          { backgroundColor: getBackgroundColor(this.quoteNumber) }
+          { backgroundColor: this.getBackgroundColor(this.quoteNumber) }
         ]}
       >
         <MyText
@@ -36,44 +36,32 @@ class MindfulQuoteTile extends React.Component {
       </View>
     );
   }
-}
 
-getBackgroundColor = function(quoteNumber) {
-  return COLOR_PRIMARY;
-  // switch (quoteNumber % 5) {
-  //   case 0:
-  //     return COLOR_ALERT;
-  //   case 1:
-  //     return COLOR_SECONDARY;
-  //   case 2:
-  //     return COLOR_TERTIARY;
-  //   case 3:
-  //     return COLOR_HIGHLIGHT;
-  //   default:
-  //     return COLOR_PRIMARY;
-  // }
-};
+  getBackgroundColor = function(quoteNumber) {
+    return COLOR_PRIMARY;
+  };
+}
 
 const styles = StyleSheet.create({
   header: {
     backgroundColor: COLOR_WHITE,
     flex: 1,
-    justifyContent: "center",
-    flexDirection: "column",
-    alignItems: "center",
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
     padding: 30,
     marginBottom: 10
   },
   quoteText: {
     flex: 1,
-    textAlign: "center",
+    textAlign: 'center',
 
     color: COLOR_WHITE,
     fontSize: 18
   },
   quoteAuthor: {
     flex: 1,
-    textAlign: "center",
+    textAlign: 'center',
     color: COLOR_WHITE,
     fontSize: 16
   }
