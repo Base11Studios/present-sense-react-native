b4d84d81fff636414d453c110f04f4723db8ce6b
Dan Bellinski <bellindj@gmail.com>
2018-12-08 08:59:48 -0500

    Don't repeat affirmations

--- redux/saga.js
+++ redux/saga.js
@@ -223,7 +223,6 @@ function scheduleAffirmationNotification(date, affirmation, id) {
     playSound: true, // (optional) default: true
     soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
     number: '10', // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
-    repeatType: 'day', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
     // actions: '[""]', // (Android only) See the doc for notification actions to know more
     date: date
   };
