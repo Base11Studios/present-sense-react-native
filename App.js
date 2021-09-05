b733e59613b9dc9898cc3d674a2accca57e65e36
Dan Bellinski <bellindj@gmail.com>
2021-09-05 09:15:20 -0400

    Bump to 61

--- App.js
+++ App.js
@@ -5,14 +5,7 @@
  */
 
 import React from 'react';
-import {
-  ActivityIndicator,
-  Platform,
-  StatusBar,
-  StyleSheet,
-  View,
-  YellowBox
-} from 'react-native';
+import { ActivityIndicator, Platform, StatusBar, StyleSheet, View, YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import PushController from './components/PushController';
@@ -23,25 +16,25 @@ import { COLOR_ALERT } from './styles/common';
 // TODO remove in next react-native stable version
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated']);
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
-          <View style={styles.container}>
-            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-            <RootNavigation />
-          </View>
-          <PushController />
-        </PersistGate>
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<ActivityIndicator />} persistor={persistor}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <RootNavigation />
+        </View>
+        <PushController />
+      </PersistGate>
+    </Provider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: COLOR_ALERT
-  }
+    backgroundColor: COLOR_ALERT,
+  },
 });
+
+export default App;
